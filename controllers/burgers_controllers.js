4a99d1ef74daa0c96028a185ba3e5cb30a89b6c3
haywah27 <hayleywahlroos15@gmail.com>
2021-02-15 21:01:20 -0800

    starting to add require paths and orm calls

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,3 +1,6 @@
 // import express
 // import burger.js
 // create router and export at end of file
+
+const express = require("express");
+const burger = require("../models/burger");
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,9 @@
 // import orm.js
 // call code from ORM functions
 // export at end of file
+
+const orm = require("../config/orm");
+
+orm.selectAll("burgers");
+orm.insertOne("burgers", "burger_name", "Double Cheese");
+orm.updateOne("burgers", "Butter Burger");
