276a6c6af64d5d1d81c6515320162f7c64749ba8
haywah27 <hayleywahlroos15@gmail.com>
2021-02-15 21:21:34 -0800

    simplifying orm.js calls

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,23 @@
-// import connection.js
-// these methods will execute in the controllers file
-// * `selectAll()`
-// * `insertOne()`
-// * `updateOne()`
-
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function(table){
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, table, function (err, result) {
+    selectAll: function(){
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
             if (err) throw err;
             console.log(result)
         });
     },
-    insertOne: function(table, columnName, newBurg){
-        var queryString = "INSERT INTO ?? SET ?? VALUES ??";
-        connection.query(queryString, [table, columnName, newBurg], function (err, result) {
+    insertOne: function(newBurg){
+        var queryString = "INSERT INTO burgers SET burger_name VALUES ??";
+        connection.query(queryString, [newBurg], function (err, result) {
             if (err) throw err;
             console.log(result)
         });
     },
-    updateOne: function(table, updateBurg){
-        var queryString = "UPDATE ?? SET devoured = 1 WHERE burger_name = ??";
-        connection.query(queryString, [table, updateBurg], function (err, result) {
+    updateOne: function(updateBurg){
+        var queryString = "UPDATE burgers SET devoured = 1 WHERE burger_name = ??";
+        connection.query(queryString, [updateBurg], function (err, result) {
             if (err) throw err;
             console.log(result)
         });
--- models/burger.js
+++ models/burger.js
@@ -4,6 +4,26 @@
 
 const orm = require("../config/orm");
 
-orm.selectAll("burgers");
+const burger = {
+    selectAll: function (table, cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        })
+    },
+    insertOne: function (table, columnName, newBurg, cb) {
+        orm.insertOne("burgers", "burger_name", "Double Cheese", function (res) {
+            cb(res);
+        })
+    },
+    updateOne: function (table, updateBurg, cb) {
+        orm.updateOne("burgers", "Butter Burger", function (res) {
+            cb(res);
+        })
+    },
+
+}
 orm.insertOne("burgers", "burger_name", "Double Cheese");
 orm.updateOne("burgers", "Butter Burger");
+}
+
+module.exports = burger;
