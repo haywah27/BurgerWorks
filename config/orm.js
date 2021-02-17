03ee52aa356dfa0bbb4bd7ca79479a5f00706ba4
haywah27 <hayleywahlroos15@gmail.com>
2021-02-17 15:08:33 -0800

    commenting on code

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,7 @@
 const connection = require("./connection.js");
 
 const orm = {
+    // select all from table query, parameters unknown at this point so set future parameters to variables
     selectAll: function(cb){
         var queryString = "SELECT * FROM burgers";
         connection.query(queryString, function (err, result) {
@@ -9,24 +10,26 @@ const orm = {
             cb(result);
         });
     },
+    // create new column in burgers table
     insertOne: function(newBurg, cb){
         var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
         connection.query(queryString, newBurg,
             function (err, result) {
                 if (err) throw err;
-                console.log(result)
+
                 cb(result);
             });
     },
+    // update burger devoured variable to true (1)
     updateOne: function(updateBurg, cb){
         var queryString = "UPDATE burgers SET ? WHERE ?";
         connection.query(queryString, [{devoured: 1}, {id: updateBurg}], 
             function (err, result) {
                 if (err) throw err;
-                console.log(result)
+
                 cb(result);
             });
-    }
+    },
 }
 
 
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,37 +6,33 @@ const express = require("express");
 const burg = require("../models/burger");
 var router = express.Router();
 
+// get all burgers in burgers table and render to handlebars
 router.get("/", function(req, res) {
    burg.selectAll(function(data){
     res.render('index', { burgers: data } );
    });
-    // console.log(res)
-    //   index is handlebars
 });
 
-
+// from the submit field, create new burger and insert into burgers table, refresh page
 router.post("/", function(req, res) {
     // console.log(typeof req.body.newBurger)
     burg.insertOne(req.body.newBurger, function(){
         res.redirect('/');
     })
-
 });
 
-
-
+// on eat it button click, update devoured to true, refresh page
 router.post("/:id", function(req, res) {
     burg.updateOne(req.params.id, function(){
         res.redirect('/');
     });
 });
 
-
+// get all items in table and stringify it for posting raw table data
 router.get("/api/burgers", function(req, res) {
     burg.selectAll(function(data){
      res.json(data);
     });
-
  });
 
 
