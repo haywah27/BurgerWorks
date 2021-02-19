097026f1e152b88381d36c11e61c1131bc7c32ba
haywah27 <hayleywahlroos15@gmail.com>
2021-02-18 21:30:11 -0800

    heroku init

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit/)
 
 ## Link to webpage
-- Coming soon...
+https://burger-works.herokuapp.com/
 
 ## Description
 This is a restaurant app that lets users input the names of burgers they'd like to eat. Whenever a user submits a burger's name, this app will display said burger on the left side of the page -- "waiting to be devoured". Each burger in the waiting area also has a "Devour it!" button. When the user clicks a devour button, the burger will move to the right side of the page.
--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
+let connection;
+// = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
   
-    // Be sure to update with your own MySQL password!
+//     // Be sure to update with your own MySQL password!
+//     password: 'yourRootPassword',
+//     database: 'burgers_db',
+//   });
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
     password: 'yourRootPassword',
-    database: 'burgers_db',
-  });
+    database: 'burgers_db'
+  })
+}
+
 
 
 // Connect to the DB
